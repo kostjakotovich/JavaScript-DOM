d538119253bad387ea991ae15276acfe28efa466
kostjakotovich <kostja.kotovich@gmail.com>
2021-03-09 16:54:03 +0200

    lesson-16

--- app.js
+++ app.js
@@ -43,3 +43,9 @@ hideBox.addEventListener('change', function(e){
     list.style.display = "initial";
   }
 });
+
+// filter books
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', function(e){
+    const term = e.target.value.toLowerCase
+})
